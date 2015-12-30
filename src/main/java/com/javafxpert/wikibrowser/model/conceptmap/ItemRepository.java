3e268709b02ceda53d457a7a51d6ba7a3a54b468
JavaFXpert <james@j1w.xyz>
2015-12-30 01:18:01 -0500

    Add more relationship (P ids)

--- src/main/java/com/javafxpert/wikibrowser/model/conceptmap/ItemRepository.java
+++ src/main/java/com/javafxpert/wikibrowser/model/conceptmap/ItemRepository.java
@@ -51,15 +51,23 @@ public interface ItemRepository extends GraphRepository<GraphItem> {
       }
     }
     else {
+      System.out.println("\n\nNeed to create method " + methodStr + "() in ItemRepository\n\n");
+      // TODO: Remove println above and decide how to report
       addRel(itemIdA, itemIdB, propId, propLabel);
     }
   }
 
   @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:INSTANCE_OF {propId:{propId}, label:{propLabel}}]->(b)")
   void addRelP31(@Param("itemIdA") String itemIdA,
-                  @Param("itemIdB") String itemIdB,
-                  @Param("propId") String propId,
-                  @Param("propLabel") String propLabel);
+                 @Param("itemIdB") String itemIdB,
+                 @Param("propId") String propId,
+                 @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:DISCOVERER_OR_INVENTOR {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP61(@Param("itemIdA") String itemIdA,
+                 @Param("itemIdB") String itemIdB,
+                 @Param("propId") String propId,
+                 @Param("propLabel") String propLabel);
 
   @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:OWNED_BY {propId:{propId}, label:{propLabel}}]->(b)")
   void addRelP127(@Param("itemIdA") String itemIdA,
@@ -69,9 +77,21 @@ public interface ItemRepository extends GraphRepository<GraphItem> {
 
   @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:NAMED_AFTER {propId:{propId}, label:{propLabel}}]->(b)")
   void addRelP138(@Param("itemIdA") String itemIdA,
-                 @Param("itemIdB") String itemIdB,
-                 @Param("propId") String propId,
-                 @Param("propLabel") String propLabel);
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:FOLLOWS {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP155(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:FOLLOWED_BY {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP156(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
 
   @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:SUBCLASS_OF {propId:{propId}, label:{propLabel}}]->(b)")
   void addRelP279(@Param("itemIdA") String itemIdA,
@@ -79,8 +99,14 @@ public interface ItemRepository extends GraphRepository<GraphItem> {
                   @Param("propId") String propId,
                   @Param("propLabel") String propLabel);
 
-  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:CAT_MAIN_TOPIC {propId:{propId}, label:{propLabel}}]->(b)")
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:CATEGORY_MAIN_TOPIC {propId:{propId}, label:{propLabel}}]->(b)")
   void addRelP301(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:IS_A_LIST_OF {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP360(@Param("itemIdA") String itemIdA,
                   @Param("itemIdB") String itemIdB,
                   @Param("propId") String propId,
                   @Param("propLabel") String propLabel);
@@ -91,19 +117,178 @@ public interface ItemRepository extends GraphRepository<GraphItem> {
                   @Param("propId") String propId,
                   @Param("propLabel") String propLabel);
 
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:USE {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP366(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:ASTRONOMICAL_BODY {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP397(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:CHILD_ASTRONOMICAL_BODY {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP398(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:QUANTITY_SYMBOL {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP416(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:SAID_TO_BE_THE_SAME_AS {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP460(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:OPPOSITE_OF {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP461(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:COLOR {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP462(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:SRGB_COLOR_HEX_TRIPLET {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP465(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:ARCHIVES_AT {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP485(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:UNICODE_CHARACTER {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP487(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:APPLIES_TO_PART {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP518(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:TYPE_OF_ORBIT {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP522(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
   @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:HAS_PART {propId:{propId}, label:{propLabel}}]->(b)")
   void addRelP527(@Param("itemIdA") String itemIdA,
                   @Param("itemIdB") String itemIdB,
                   @Param("propId") String propId,
                   @Param("propLabel") String propLabel);
 
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:TIME_OF_DISCOVERY {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP575(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:HIGHEST_POINT {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP610(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:LOCATED_ON_TERRAIN_FEATURE {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP706(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
   @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:APPROVED_BY {propId:{propId}, label:{propLabel}}]->(b)")
   void addRelP790(@Param("itemIdA") String itemIdA,
-                 @Param("itemIdB") String itemIdB,
-                 @Param("propId") String propId,
-                 @Param("propLabel") String propLabel);
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
 
-  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:R {propId:{propId}, label:{propLabel}}]->(b)")
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:TOPIC_MAIN_CATEGORY {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP910(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:NOTATION {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP913(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:PATENT_NUMBER {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1246(@Param("itemIdA") String itemIdA,
+                  @Param("itemIdB") String itemIdB,
+                  @Param("propId") String propId,
+                  @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:FACET_OF {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1269(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:PROVED_BY {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1318(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:DESCRIBED_BY_SOURCE {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1343(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:SHAPE {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1419(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:SOURCING_CIRCUMSTANCES {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1480(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:NAME_IN_KANA {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1814(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:DIFFERENT_FROM {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP1889(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:HISTORY_OF_TOPIC {propId:{propId}, label:{propLabel}}]->(b)")
+  void addRelP2184(@Param("itemIdA") String itemIdA,
+                   @Param("itemIdB") String itemIdB,
+                   @Param("propId") String propId,
+                   @Param("propLabel") String propLabel);
+
+
+
+
+  @Query("MATCH (a:Item {itemId:{itemIdA}}), (b:Item {itemId:{itemIdB}}) MERGE (a)-[:r {propId:{propId}, label:{propLabel}}]->(b)")
   void addRel(@Param("itemIdA") String itemIdA,
               @Param("itemIdB") String itemIdB,
               @Param("propId") String propId,
